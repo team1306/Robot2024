d2e26b1fce5466e7879b0513f8e9d376f9ea536f
Jiamu Chai <goatesochai@gmail.com>
2024-01-31 20:00:35 -0600

    further along, but still quite undone

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -12,6 +12,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.ReplanningConfig;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -19,6 +20,7 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelPositions;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.MotorUtil;
 import frc.robot.util.Utilities;
@@ -39,7 +41,10 @@ public class DriveTrain extends SubsystemBase{
 
     private RelativeEncoder rEncoder, lEncoder;
 
-    private DifferentialDriveOdometry odo;
+    private DifferentialDrivePoseEstimator poseEstimator;
+
+    private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(TRACK_WIDTH);
+    //private final AHRS gyro = new AHRS(SPI.Port.kMXP);
 
     private ChassisSpeeds lastSpeeds;
 
@@ -60,7 +65,7 @@ public class DriveTrain extends SubsystemBase{
         rEncoder.setPosition(0);
         lEncoder.setPosition(0);
 
-        odo = new DifferentialDriveOdometry(new Rotation2d(), lEncoder.getPosition(), rEncoder.getPosition());
+        poseEstimator = new DifferentialDrivePoseEstimator(kinematics, new Rotation2d(), 0, 0, getStartingPose());
 
         //Pathplanner configuration
         AutoBuilder.configureLTV(
@@ -116,8 +121,7 @@ public class DriveTrain extends SubsystemBase{
     }
 
     public void drive(ChassisSpeeds speeds){
-        DifferentialDriveKinematics kinematics =
-        new DifferentialDriveKinematics(TRACK_WIDTH);
+        
 
         lastSpeeds = speeds;
 
@@ -127,19 +131,23 @@ public class DriveTrain extends SubsystemBase{
     }
 
     private Pose2d getPose(){
-        return odo.getPoseMeters();
+        return null;//poseEstimator.getEstimatedPosition()
     }
 
     private void resetPose(Pose2d pose){
-        odo.resetPosition(new Rotation2d(), new DifferentialDriveWheelPositions(0,0), pose);
+        poseEstimator.resetPosition(new Rotation2d(), new DifferentialDriveWheelPositions(0,0), pose);
     }
 
     private ChassisSpeeds getCurrentSpeeds(){
         return lastSpeeds;
     }
 
-    // @Override
-    // public void periodic() {
-    //     odo.update(null, null);
-    // }
+    @Override
+    public void periodic() {
+        poseEstimator.update(null, null);
+    }
+
+    public Pose2d getStartingPose() {
+        return new Pose2d(); // TODO: GET FROM AUTO
+    }
 }
