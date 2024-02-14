6a1a2c7561da5f08919c89f0c7fd76ce52e7b644
Jiamu Chai <bradley.chumanov@gmail.com>
2024-02-13 19:12:17 -0600

    fix bad naming

--- src/main/java/frc/robot/commands/drive/ShooterDriveCommand.java
+++ src/main/java/frc/robot/commands/drive/ShooterDriveCommand.java
@@ -26,39 +26,51 @@ public class ShooterDriveCommand extends Command{
 
     private boolean finished = false;
     private double deadbandValue = 0.05;
-    public static double P = 1, I = 0, D = 0.1;
+    public static double kP = 1, kI = 0, kD = 0;
 
     public ShooterDriveCommand(DriveTrain driveTrain, Shooter shooter, NoteIndexingCommand noteIndexCommand){
         this.driveTrain = driveTrain;
         this.shooter = shooter;
         this.noteIndexCommand = noteIndexCommand;
+        this.rotationController = new PIDController(kP, kI, kD);
         this.addRequirements(driveTrain);
-        this.rotationController = new PIDController(P, I, D);
+         
+        SmartDashboard.putNumber("Shooter Drive kP", kP);
+        SmartDashboard.putNumber("Shooter Drive kI", kI);
+        SmartDashboard.putNumber("Shooter Drive kD", kD);
         SmartDashboard.putNumber("Shooter Auto Deadband", deadbandValue);
     }
 
     @Override
     public void execute(){
-        rotationController.setPID(P, I, D);
+        kP = SmartDashboard.getNumber("Shooter Drive kP", 1);
+        kI = SmartDashboard.getNumber("Shooter Drive kI", 0);
+        kD = SmartDashboard.getNumber("Shooter Drive kD", 0);
         deadbandValue = SmartDashboard.getNumber("Shooter Auto Deadband", 1);
+
+        rotationController.setPID(kP, kI, kD);
         Pose2d botPose = LimelightHelpers.getBotPose2d(LIMELIGHT_NAME);
         Translation2d targetPos = Utilities.getSpeaker().minus(botPose.getTranslation());
         Rotation2d angle = Rotation2d.fromRadians(Math.atan2(targetPos.getY(), targetPos.getX()));
         Rotation2d robotAngle = botPose.getRotation();
-        final double delta = MathUtil.applyDeadband((angle.minus(robotAngle).plus(Rotation2d.fromDegrees(Utilities.isRedAlliance() ? 180 : 0))).getDegrees(), deadbandValue);
-        if (delta == 0)
-            finished = true;
-        else {
-            driveTrain.arcadeDrive(0, MotorUtil.clampPercent(rotationController.calculate(delta)));
-        }
+
+        final double delta = MathUtil.applyDeadband(
+            (angle.minus(robotAngle).plus(Rotation2d.fromDegrees(Utilities.isRedAlliance() ? 180 : 0))).getDegrees(), deadbandValue);
+        
+        if (delta == 0) finished = true;
+        else driveTrain.arcadeDrive(0, MotorUtil.clampPercent(rotationController.calculate(delta)));
+        
     }
 
     @Override
-    public boolean isFinished(){
-        if(finished)
-            CommandScheduler.getInstance().schedule(
-                noteIndexCommand.alongWith(shooter.getToggleShooterCommand(() -> 1.0)
-                .withTimeout(NoteIndexingCommand.TIME_AFTER_SHOT_MS + NoteIndexingCommand.TIME_BEFORE_SHOT_MS)));
+    public boolean isFinished(){            
         return finished;
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        CommandScheduler.getInstance().schedule(
+            noteIndexCommand.alongWith(shooter.getToggleShooterCommand(() -> 1.0)
+            .withTimeout(NoteIndexingCommand.TIME_AFTER_SHOT_MS + NoteIndexingCommand.TIME_BEFORE_SHOT_MS)));
     }
 }
