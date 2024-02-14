cf089532e93c73234da2cf4d18f5fdaba4eef623
Jiamu Chai <bradley.chumanov@gmail.com>
2024-02-13 18:16:15 -0600

    remove test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,22 +4,12 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.util.MotorUtil;
-import static frc.robot.Constants.*;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-  private CANSparkMax motor;
-  private XboxController tController = new XboxController(1);
   private RobotContainer m_robotContainer;
   @Override
   public void robotInit() {
@@ -65,7 +55,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    // motor.set(MathUtil.applyDeadband(tController.getLeftY(), 0.05));
   }
 
   @Override
