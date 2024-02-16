7de975b1fdec729d2d1b97e0e769398d04dbc2a3
Jiamu Chai <bradley.chumanov@gmail.com>
2024-02-15 20:13:54 -0600

    arm auto code

--- src/main/java/frc/robot/commands/arm/MoveArmCommand.java
+++ src/main/java/frc/robot/commands/arm/MoveArmCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.arm;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Arm;
@@ -11,6 +12,7 @@ public class MoveArmCommand extends Command {
     private final Arm arm;
     private double minAngle = -1e+9;
     private double maxAngle = 1e+9;
+        private double currentAngle = 0;
 
     private final DoubleSupplier rotationSupplier;
 
@@ -20,21 +22,22 @@ public class MoveArmCommand extends Command {
         this.addRequirements(arm);
 
         SmartDashboard.putNumber("Arm Min Angle", 0.00);
+        SmartDashboard.putNumber("current angle", 0);
         SmartDashboard.putNumber("Arm Max Angle", 90);
     }
 
     @Override
     public void initialize(){
-        arm.setControlMode(Arm.ControlMode.MANUAL);
+        arm.setControlMode(Arm.ControlMode.AUTOMATIC);
     }
 
     @Override
     public void execute(){
+        currentAngle = SmartDashboard.getNumber("current angle", currentAngle);
         minAngle = SmartDashboard.getNumber("Arm Min Angle", minAngle);
         maxAngle = SmartDashboard.getNumber("Arm Max Angle", maxAngle);
 
-        final double armAngle = arm.getCurrentAngle().getDegrees();
         // arm.setManualPower(armAngle < minAngle || armAngle > maxAngle ? 0 : rotationSupplier.getAsDouble());
-        arm.setManualPower(rotationSupplier.getAsDouble() * .1);
+        arm.setTargetAngle(Rotation2d.fromDegrees(currentAngle));
     }
 }
