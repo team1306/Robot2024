1358b9e6aad73cada3314dcb09ee01e68eeb61ab
Connor Mulligan <cgmulligan33@gmail.com>
2024-02-27 19:21:30 -0600

    fixed bugs in example impl

--- src/main/java/frc/robot/auto/FarRingsFromCloseMiddle.java
+++ src/main/java/frc/robot/auto/FarRingsFromCloseMiddle.java
@@ -5,15 +5,16 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.vision.NoteDetector;
 
 public class FarRingsFromCloseMiddle extends SequentialCommandGroup {
     public FarRingsFromCloseMiddle(NoteDetector detector) {
         addCommands(
             AutoBuilder.followPath(PathPlannerPath.fromPathFile("Close-Middle to Far-1")),
             detector.notePresenceCommandSwitcher(
+                1,
                 AutoBuilder.followPath(PathPlannerPath.fromPathFile("Close-Middle to Far-1")),
                 AutoBuilder.followPath(PathPlannerPath.fromPathFile("Close-Middle to Far-1"))
-
             )
         );
     }
