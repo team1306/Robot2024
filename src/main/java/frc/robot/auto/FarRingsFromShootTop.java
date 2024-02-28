d0c35582ef1310898b8914a0a52adad148ada6c6
Ethan Sanders <essanders@wisc.edu>
2024-02-28 14:02:05 -0600

    updates to note detection and autonomous framework

--- src/main/java/frc/robot/auto/FarRingsFromShootTop.java
+++ src/main/java/frc/robot/auto/FarRingsFromShootTop.java
@@ -7,18 +7,19 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.vision.NoteDetector;
+import frc.robot.subsystems.vision.NoteDetector.Checkpoints;
 
 public class FarRingsFromShootTop extends SequentialCommandGroup {
     public FarRingsFromShootTop(NoteDetector detector) {
         addCommands(
             //Collect 1 and 2
-            detector.read(),
+            detector.read(Checkpoints.TOP),
             detector.notePresenceCommandSwitcher(1,
                 //if note 1 present
                 new SequentialCommandGroup(
                     AutoBuilder.followPath(PathPlannerPath.fromPathFile("Shoot-Top to Far-1")),
                     AutoBuilder.followPath(PathPlannerPath.fromPathFile("Far-1 to Shoot-Top")),
-                    detector.read(),
+                    detector.read(Checkpoints.TOP),
                     detector.notePresenceCommandSwitcher(2,
                         //if note 2 present
                         new SequentialCommandGroup(
--- src/main/java/frc/robot/subsystems/vision/NoteDetector.java
+++ src/main/java/frc/robot/subsystems/vision/NoteDetector.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.vision;
 
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public interface NoteDetector {
@@ -9,17 +12,29 @@ public interface NoteDetector {
         UNKNOWN
     }
 
+    public enum Checkpoints {
+        TOP(PathPlannerPath.fromPathFile("Shoot-Top to Far-2.path").getStartingDifferentialPose()),
+        TOP_MIDDLE(PathPlannerPath.fromPathFile("Scan-Top to Far-3.path").getStartingDifferentialPose()),
+        BOTTOM_MIDDLE(PathPlannerPath.fromPathFile("Scan-Bottom to Far-2.path").getStartingDifferentialPose());
+
+        final Pose2d pose;
+
+        private Checkpoints(Pose2d pose) {
+            this.pose = pose;
+        }
+    }
+
     /**
      * Gets state of board with respect to notes present on the field.
      * @return Array of length 5, 0 index is top of board
      */
     State[] getNoteState();
-
+    
     /**
      * read note position
      * @return command to read note position
      */
-    Command read();
+    Command read(Checkpoints checkpoint);
 
     /**
      * Switches command based off of presence of a note
