ff82590d1a0fa3165d587574248448c67d0987fe
Bradley Chumanov <bradley.chumanov@gmail.com>
2024-03-03 16:42:41 -0600

    add speedlookup table

--- src/main/java/frc/robot/commands/shooter/ToggleShooterCommand.java
+++ src/main/java/frc/robot/commands/shooter/ToggleShooterCommand.java
@@ -1,8 +1,8 @@
 package frc.robot.commands.shooter;
 
+import java.util.HashMap;
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 import frc.robot.util.MotorUtil;
@@ -11,23 +11,27 @@ public class ToggleShooterCommand extends Command{
     private final Shooter shooter;
     private final DoubleSupplier speedSupplier;
     private final DoubleSupplier angleSupplier;
-    private double shooterSpeedAngleRatio = 22;
+    private final HashMap<Integer, Double> shooterSpeedLookupFromAngle = new HashMap<>() {{
+        put(0, .79);
+        put(1, .79);
+        put(2, .79);
+        put(3, .89);
+        put(4, .89);
+        put(5, 1.0);
+        put(6, 1.0);
+        put(7, 1.0);
+        put(8, 1.0);
+    }};
 
     public ToggleShooterCommand(DoubleSupplier speedSupplier, DoubleSupplier angleSupplier, Shooter shooter){
         addRequirements(shooter);
         this.shooter = shooter;
         this.speedSupplier = speedSupplier;
         this.angleSupplier = angleSupplier;
-        SmartDashboard.putNumber("Shooter Speed Angle Ratio", shooterSpeedAngleRatio);
     }
     @Override
     public void initialize(){
-        shooter.setTargetSpeed(MotorUtil.clampPercent(speedSupplier.getAsDouble() * (angleSupplier.getAsDouble() / shooterSpeedAngleRatio))); 
-    }
-
-    @Override
-    public void execute(){
-        shooterSpeedAngleRatio = SmartDashboard.getNumber("Shooter Speed Angle Ratio", shooterSpeedAngleRatio);
+        shooter.setTargetSpeed(MotorUtil.clampPercent(speedSupplier.getAsDouble() * shooterSpeedLookupFromAngle.get(((int)angleSupplier.getAsDouble()) / 10))); 
     }
     @Override
     public void end(boolean interrupted){
