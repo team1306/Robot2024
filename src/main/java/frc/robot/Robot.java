ea83244e257c205b1993ae98f5f1253b1ba91593
Connor Mulligan <cgmulligan33@gmail.com>
2024-03-09 12:59:02 -0600

    no changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.arm.DebugArmCommand;
-import frc.robot.subsystems.vision.SwitchableDriverCam;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand, m_armDebugCommand;
