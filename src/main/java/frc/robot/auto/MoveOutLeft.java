5aa84c7137476f886e200299cf74ad910d465253
Connor Mulligan <cgmulligan33@gmail.com>
2024-03-09 14:45:56 -0600

    added shoot command and finished fallback autos

--- src/main/java/frc/robot/auto/MoveOutLeft.java
+++ src/main/java/frc/robot/auto/MoveOutLeft.java
@@ -1,25 +1,35 @@
 package frc.robot.auto;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.arm.MoveArmToSetpointCommand;
+import frc.robot.commands.intake.IntakeIndexCommand;
+import frc.robot.commands.shooter.ToggleShooterCommand;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
 public class MoveOutLeft extends SequentialCommandGroup {
     private DriveTrain driveTrain = new DriveTrain(null);
-    public MoveOutLeft(Shooter shooter, Arm arm) {
+    public MoveOutLeft(Shooter shooter, Arm arm, Intake intake) {
 
         System.out.println("Running Auto");
-
-        addCommands(
-            new MoveArmToSetpointCommand(arm, Arm.Setpoint.SHOOT_CLOSE),
-            //shoot
-
-            new ParallelDeadlineGroup(new WaitCommand(2),
-                driveTrain.driveBySetpointPercentagesCommand(0.15,0.3)
+        final Command shooterCommand = new ToggleShooterCommand(() -> 1, () -> arm.getCurrentAngle().getDegrees(), shooter);
+        addCommands( //all commands run at once
+            shooterCommand, //turns on shooter
+            new SequentialCommandGroup( //following commands run in sequence
+                new MoveArmToSetpointCommand(arm, Arm.Setpoint.SHOOT_CLOSE), //aim
+                new WaitCommand(1),
+                new IntakeIndexCommand(intake), //fire
+                new InstantCommand(() -> shooterCommand.cancel()), //turn off shooter
+                new MoveArmToSetpointCommand(arm, Arm.Setpoint.DOWN), //arm down
+                new ParallelDeadlineGroup(new WaitCommand(2), //drive out
+                    driveTrain.driveBySetpointPercentagesCommand(0.3,0.15)
+                )
             )
         );
     }
--- src/main/java/frc/robot/auto/MoveOutMid.java
+++ src/main/java/frc/robot/auto/MoveOutMid.java
@@ -1,26 +1,36 @@
 package frc.robot.auto;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.arm.MoveArmToSetpointCommand;
+import frc.robot.commands.intake.IntakeIndexCommand;
+import frc.robot.commands.shooter.ToggleShooterCommand;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
-public class MoveOutMid extends SequentialCommandGroup {
+public class MoveOutMid extends ParallelCommandGroup {
     private DriveTrain driveTrain = new DriveTrain(null);
-    public MoveOutMid(Shooter shooter, Arm arm) {
+    public MoveOutMid(Shooter shooter, Arm arm, Intake intake) {
 
         System.out.println("Running Auto");
-
-        addCommands(
-            new MoveArmToSetpointCommand(arm, Arm.Setpoint.SHOOT_CLOSE),
-            //shoot
-
-            new ParallelDeadlineGroup(new WaitCommand(2),
-                driveTrain.driveBySetpointPercentagesCommand(0.2,0.2)
+        final Command shooterCommand = new ToggleShooterCommand(() -> 1, () -> arm.getCurrentAngle().getDegrees(), shooter);
+        addCommands( //all commands run at once
+            shooterCommand, //turns on shooter
+            new SequentialCommandGroup( //following commands run in sequence
+                new MoveArmToSetpointCommand(arm, Arm.Setpoint.SHOOT_CLOSE), //aim
+                new WaitCommand(1),
+                new IntakeIndexCommand(intake), //fire
+                new InstantCommand(() -> shooterCommand.cancel()), //turn off shooter
+                new MoveArmToSetpointCommand(arm, Arm.Setpoint.DOWN), //arm down
+                new ParallelDeadlineGroup(new WaitCommand(2), //drive out
+                    driveTrain.driveBySetpointPercentagesCommand(0.2,0.2)
+                )
             )
         );
     }
