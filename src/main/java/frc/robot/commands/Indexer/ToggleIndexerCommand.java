1c5b752f452ee03249d9c120341ce6b6b3d78e2c
Bradley Chumanov <bradley.chumanov@gmail.com>
2024-07-16 18:39:15 -0500

    new commands

---
+++ src/main/java/frc/robot/commands/Indexer/ToggleIndexerCommand.java
@@ -0,0 +1,5 @@
+package frc.robot.commands.Indexer;
+
+public class ToggleIndexerCommand {
+    
+}
---
+++ src/main/java/frc/robot/commands/Shooter/ToggleShooterCommand.java
@@ -0,0 +1,5 @@
+package frc.robot.commands.Shooter;
+
+public class ToggleShooterCommand {
+    
+}
