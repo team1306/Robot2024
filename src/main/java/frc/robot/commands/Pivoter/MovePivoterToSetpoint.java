da2577be29a841340448296629c57684d9159287
Bradley Chumanov <bradley.chumanov@gmail.com>
2024-07-16 19:09:15 -0500

    add pivoter commands

---
+++ src/main/java/frc/robot/commands/Pivoter/MovePivoterToSetpoint.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.Pivoter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Pivoter;
+import frc.robot.subsystems.utils.PivoterSetpoint;
+
+public class MovePivoterToSetpoint extends Command{
+
+    private final Pivoter pivoter;
+    private final PivoterSetpoint setpoint;
+    public MovePivoterToSetpoint (Pivoter pivoter, PivoterSetpoint setpoint){
+        this.pivoter = pivoter;
+        this.setpoint = setpoint;
+    }
+
+    @Override
+    public void initialize(){
+        pivoter.setTargetAngle(setpoint.getAngle());
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true;
+    }
+}
