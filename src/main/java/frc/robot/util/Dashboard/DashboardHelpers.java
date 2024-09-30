20aad51ec6c7e1ded7317f523efd8fd03799d44e
NukeMinecart <bradley.chumanov@gmail.com>
2024-09-30 09:32:47 -0500

    wrote the documentation for the new DashboardHelpers

--- src/main/java/frc/robot/util/Dashboard/DashboardHelpers.java
+++ src/main/java/frc/robot/util/Dashboard/DashboardHelpers.java
@@ -1,6 +1,7 @@
 package frc.robot.util.Dashboard;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 import lombok.SneakyThrows;
 
 import java.lang.reflect.Field;
@@ -10,9 +11,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This class is a set of tools to write less code for SmartDashboard related methods. It primarily uses annotations to achieve this.
+ */
 public class DashboardHelpers {
     private static final Set<Object> classesToUpdate = new HashSet<>();
 
+    /**
+     * Adds a class to check for any {@link GetValue} or {@link PutValue} annotations. 
+     * This will update the fields with {@link GetValue} from the value on SmartDashboard.
+     * This will put the value of any fields or call any methods with {@link PutValue} to SmartDashboard
+     * @param instance the instance of the class to check
+     */
     public static void updateClass(Object instance) {
         Arrays.stream(instance.getClass().getDeclaredFields())
                 .filter(field -> field.isAnnotationPresent(GetValue.class)).forEach(field -> {
@@ -39,15 +49,31 @@ public class DashboardHelpers {
 
     }
 
+    /**
+     * Updates all the fields and methods in the registered classes by either putting or getting a specific value. 
+     * This method should be called only once in {@link Robot#robotPeriodic()}
+     */
     public static void update() {
         classesToUpdate.forEach(DashboardHelpers::updateClass);
     }
 
+    /**
+     * Gets the current value of a field
+     * @param instance the instance of the class with the field in it
+     * @param field the field to get from
+     * @return the value of the field
+     */
     @SneakyThrows(IllegalAccessException.class)
     private static Object getFieldValue(Object instance, Field field) {
         return field.get(instance);
     }
 
+    /**
+     * Gets the value of a method
+     * @param instance the instance of the class with the method in it
+     * @param method the method to get the value from
+     * @return the value of the method
+     */
     @SneakyThrows({IllegalAccessException.class, InvocationTargetException.class})
     private static Object getMethodValue(Object instance, Method method) {
         return method.invoke(instance);
@@ -71,6 +97,11 @@ public class DashboardHelpers {
         }
     }
 
+    /**
+     * Handles the putting of an {@link Object} to SmartDashboard without explicitly writing the type
+     * @param key the key to put to SmartDashboard
+     * @param value the value to put to SmartDashboard. This must be one of the valid types ({@link Double}, {@link String}, {@link Boolean})
+     */
     public static void putValue(String key, Object value) {
         if (value instanceof Double) {
             SmartDashboard.putNumber(key, (double) value);
