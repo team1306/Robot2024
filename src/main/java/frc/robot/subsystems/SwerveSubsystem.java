290920961b4fb840157037c2f523b2af6b464da3
NukeMinecart <bradley.chumanov@gmail.com>
2024-11-05 22:21:38 -0600

    choreo now works! (needed to update the library)

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -4,16 +4,14 @@
 
 package frc.robot.subsystems;
 
-import com.choreo.lib.Choreo;
-import com.choreo.lib.ChoreoControlFunction;
-import com.choreo.lib.ChoreoTrajectory;
+import choreo.Choreo;
+import choreo.auto.AutoFactory;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -29,7 +27,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import frc.robot.Constants;
@@ -43,6 +40,8 @@ import org.photonvision.targeting.PhotonPipelineResult;
 import swervelib.SwerveController;
 import swervelib.SwerveDrive;
 import swervelib.SwerveDriveTest;
+import swervelib.SwerveModule;
+import swervelib.parser.PIDFConfig;
 import swervelib.parser.SwerveDriveConfiguration;
 import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
@@ -179,6 +178,7 @@ public class SwerveSubsystem extends SubsystemBase
    */
   public Command aimAtSetpoint(Rotation2d rotation, Rotation2d tolerance)
   {
+    //TODO Likely NOT WORKING BECAUSE DRIVE RESETS BACK TO WHAT IT LAST WAS
     SwerveController controller = swerveDrive.getSwerveController();
     return run(
         () -> {
@@ -389,24 +389,28 @@ public class SwerveSubsystem extends SubsystemBase
   @Override
   public void periodic()
   {
-    // if (pushPID) {
-    //   for (SwerveModule swerveModule : swerveDrive.getModules()) {
-    //     swerveModule.setDrivePIDF(new PIDFConfig(driveP, driveI, driveD, driveF, driveIZ));
-    //     swerveModule.setAnglePIDF(new PIDFConfig(angleP, angleI, angleD, angleF, angleIZ));
-    //   }
+     if (pushPID) {
+       for (SwerveModule swerveModule : swerveDrive.getModules()) {
+         swerveModule.setDrivePIDF(new PIDFConfig(driveP, driveI, driveD, driveF, driveIZ));
+         swerveModule.setAnglePIDF(new PIDFConfig(angleP, angleI, angleD, angleF, angleIZ));
+       }
 
-    //   pushPID = false;
-    // }
+       pushPID = false;
+     }
     swerveDrive.getSwerveController().thetaController.setPID(headingP, headingI, headingD);
   }
 
   public Command getAutoCommand(){
-    ChoreoTrajectory trajectory = Choreo.getTrajectory("TestPath");
-    PIDController xController = new PIDController(0, 0, 0);
-    PIDController yController = new PIDController(0, 0, 0);
-    PIDController rotationController = new PIDController(0, 0, 0);
-
-    return Choreo.choreoSwerveCommand(trajectory, this::getPose, xController, yController,rotationController, this::drive, this::isRedAlliance, this);
+    var trajectory = Choreo.loadTrajectory("TestPath");
+    if(trajectory.isEmpty()) return new InstantCommand();
+    
+    //TODO verify that this is the correct way to create the auto factory
+    var builder = Choreo.createAutoFactory(this, this::getPose, (pose, sampleType) -> drive(sampleType.getChassisSpeeds())
+    , this::isRedAlliance, new AutoFactory.AutoBindings());
+    
+    resetOdometry(trajectory.get().getInitialPose(isRedAlliance()));
+    
+    return builder.trajectoryCommand(trajectory.get());
   }
 
   @Override
